83ac6e42a8ed2ad144263492342050f70b501b08
Florent MARCAILLE <f.marcaille@yahoo.fr>
2017-03-27 14:30:24 +0200

    main cleaning

--- main.c
+++ main.c
@@ -9,24 +9,50 @@
 
 /* ***************************************** MAIN **************************************/
 
+void affichageConsole(char *metroDessin,int position,Pile *metro,File *stationA,File *stationB){
+    int i;
+
+    metroDessin[position]='=';
+    metroDessin[position+1]='o';
+
+    printf("Nombre de personnes dans le metro : %d\n", tailleMetro(&metro));
+    for(i=0;i< tailleMetro(&metro);i++){
+        printf("|");
+    }
+    printf("\n");
+
+    printf("Nombre de personnes dans la station A : %d\n", tailleStation(&stationA));
+    for(i=0;i< tailleStation(&stationA);i++){
+        printf("|");
+    }
+    printf("\n");
+
+    printf("Nombre de personnes dans la station B : %d\n", tailleStation(&stationB));
+    for(i=0;i< tailleStation(&stationB);i++){
+        printf("|");
+    }
+    printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+
+    printf("%s\n",metroDessin);
+
+    Sleep(300);
+}
+
 int main()
 {
     printf("*************Bienvenue sur la simulation d'un mini métro*************\n\n");
+
     char metroDessin[] = "{A}=>========={B}";
     int direction = 0; // 0 droite, 1 gauche
     int position = 4; // position de départ dans la chaine du métro
     int nbPersonneChangement = 0;
-    int i;
-
 
     listeChainee *listeStation = NULL;
 
     File *stationA = NULL;
-
     File *stationB = NULL;
 
     ajouterEnFin(listeStation,stationA);
-
     ajouterEnFin(listeStation,stationB);
 
     Pile *metro = NULL;
@@ -38,25 +64,7 @@ int main()
                     remplissageStation(&stationA,position);
                 }
                 remplissageStation(&stationB,position);
-                metroDessin[position]='=';
-                metroDessin[position+1]='>';
-                printf("Nombre de personnes dans le metro : %d\n", tailleMetro(&metro));
-                for(i=0;i< tailleMetro(&metro);i++){
-                    printf("|");
-                }
-                printf("\n");
-                printf("Nombre de personnes dans la station A : %d\n", tailleStation(&stationA));
-                for(i=0;i< tailleStation(&stationA);i++){
-                    printf("|");
-                }
-                printf("\n");
-                printf("Nombre de personnes dans la station B : %d\n", tailleStation(&stationB));
-                for(i=0;i< tailleStation(&stationB);i++){
-                    printf("|");
-                }
-                printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
-                printf("%s\n",metroDessin);
-                Sleep(300);
+                affichageConsole(metroDessin,position,metro,stationA,stationB);
             }
             videMetro(&metro);
             for(nbPersonneChangement = 0;nbPersonneChangement<tailleStation(&stationB);nbPersonneChangement++){
@@ -71,25 +79,7 @@ int main()
                     remplissageStation(&stationA,position);
                 }
                 remplissageStation(&stationB,position);
-                metroDessin[position]='=';
-                metroDessin[position-1]='<';
-                printf("Nombre de personnes dans le metro : %d\n", tailleMetro(&metro));
-                for(i=0;i< tailleMetro(&metro);i++){
-                    printf("|");
-                }
-                printf("\n");
-                printf("Nombre de personnes dans la station A : %d\n", tailleStation(&stationA));
-                for(i=0;i< tailleStation(&stationA);i++){
-                    printf("|");
-                }
-                printf("\n");
-                printf("Nombre de personnes dans le station B : %d\n", tailleStation(&stationB));
-                for(i=0;i< tailleStation(&stationB);i++){
-                    printf("|");
-                }
-                printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
-                printf("%s\n",metroDessin);
-                Sleep(300);
+                affichageConsole(metroDessin,position,metro,stationA,stationB);
             }
             videMetro(&metro);
             for(nbPersonneChangement = 0;nbPersonneChangement<tailleStation(&stationA);nbPersonneChangement++){
